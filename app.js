1b47f560a75fd0965cc9f898cae61843d98d311f
Sonia <iuliia.lynnik@gmail.com>
2022-05-18 13:21:38 -0600

    the final project complete

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-// Require the necessary discord.js classes 
-const { Client, Intents} = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
+const config = require('./config.json');
 
-// Create a new client instance 
-const client = new Client ({ intents: [Intents.FLAGS.GUILDS] });
-
-//When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Login to Discord with your client's token 
-client.login(token);
+client.on("guildMemberAdd", (member) =>{ 
+  const channel = member.guild.channels.cache.find(channel => channel.name === "general") 
+  if (!channel) return; 
+  const { MessageEmbed } = require('discord.js');  
+  const exampleEmbed = new MessageEmbed() 
+  .setColor('#FF00FF') 
+  .setTitle('New Member joined!!') 
+  .setDescription(`${member.user.tag} has joined the server! Give them a warm welcome!`)
+
+  channel.send({ embeds: [exampleEmbed] });
+
+});
+
+client.login(config.token);
